a948df402361231328a22cc4e130c1b46baae867
lprinz1 <lprinz1@lion.lmu.edu>
2022-11-30 12:57:36 -0800

    added seach bar to top and added background to cards

--- mygainz/src/pages/excercises.js
+++ mygainz/src/pages/excercises.js
@@ -37,17 +37,17 @@ export default function Excercises() {
         <h1 className="page-header">Excercises <span className="page-header text-gray-500">(Page {page + 1})</span></h1>
         <h2 className="page-header text-gray-400">{excercises.length} Results</h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-12 gap-y-8 overflow-scroll scrollbar-hide h-full">
+      <div className="h-16 flex justify-end items-center relative rounded-lg border-2 bg-gray-100 border-gray-200 px-4 focus:outline-blue-500">
+          <input onChange={(e) => setSearch(e.target.value)} placeholder="Filter by name, muscle group, or equipment..." className="w-full h-full focus:outline-none text-gray-600 bg-gray-100" />
+          <IoSearch className="fill-gray-400" />
+        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 overflow-scroll scrollbar-hide h-full">
         {excercises.slice(page, page + 12).map(excercise => {
           return <ExcerciseCard key={excercise.id} excercise={excercise} />;
         })}
       </div>
       <div className="flex justify-between">
         <button className="btn-primary" onClick={prevPage}><RxDoubleArrowLeft />Prev</button>
-        <div className="w-2/3 h-12 mx-8 flex justify-end items-center relative rounded-lg border-2 bg-gray-100 border-gray-200 px-4 focus:outline-blue-500">
-          <input onChange={(e) => setSearch(e.target.value)} placeholder="Filter by name, muscle group, or equipment..." className="w-full h-full focus:outline-none text-gray-600 bg-gray-100" />
-          <IoSearch className="fill-gray-400" />
-        </div>
         <button className="btn-primary lg:col-start-4" onClick={nextPage}>Next<RxDoubleArrowRight /></button>
       </div>
     </div>

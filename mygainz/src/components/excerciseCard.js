8c1da33985382a63eec4a39475ce0cb4c2ff7ebd
lprinz1 <notloosh@gmail.com>
2022-12-05 12:27:18 -0800

    fixed excercise box height

--- mygainz/src/components/excerciseCard.js
+++ mygainz/src/components/excerciseCard.js
@@ -1,5 +1,5 @@
 export default function ExcerciseCard({ excercise }) {
-  return <div className="w-80 md:w-full bg-gray-100 p-4 rounded-lg">
+  return <div className="w-80 aspect-square md:w-full bg-gray-100 p-4 rounded-lg">
     <p className="text-gray-500 font-sans capitalize font-medium truncate text-md">{excercise.name}</p>
     <p className="text-gray-400 font-sans capitalize font-regular truncate text-md">{excercise.equipment}</p>
     <p className="text-gray-400 font-sans capitalize font-regular truncate text-md mb-4">Target Muscle: <span className="text-red-600">{excercise.target}</span></p>

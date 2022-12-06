ee547d6daaa338e375cbf2dbc30515836ed6b318
Ryan Cordero <rcorder4@lion.lmu.edu>
2022-12-05 20:20:56 -0800

    updating programs

--- mygainz/src/components/Routine.js
+++ mygainz/src/components/Routine.js
@@ -1,40 +1,40 @@
 export default function Routine({ routine }) {
   return (
-    <div class=" md:w-full bg-gray-100 p-4 rounded-lg">
+    <div class=" md:w-full bg-gray-200 p-4 rounded-lg outline outline-2 outline-slate-400 py-3">
       <p class="text-gray-900 font-sans capitalize font-medium truncate text-md"></p>
       <p class="text-gray-400 font-sans capitalize font-regular truncate text-md"></p>
       {routine.name}
       <div className="flex-wrap py-8 px-6 flex flex text-center items-center w-full gap-6">
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6">
+        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
           {" "}
           Day 1: <br></br> {routine.day1}{" "}
         </button>
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6">
+        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
           {" "}
           Day 2: <br></br>
           {routine.day2}{" "}
         </button>
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6">
+        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
           {" "}
           Day 3: <br></br>
           {routine.day3}{" "}
         </button>
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6">
+        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
           {" "}
           Day 4: <br></br>
           {routine.day4}{" "}
         </button>
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6">
+        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
           {" "}
           Day 5: <br></br>
           {routine.day5}{" "}
         </button>
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6">
+        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
           {" "}
           Day 6: <br></br>
           {routine.day6}{" "}
         </button>
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6">
+        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
           {" "}
           Day 7: <br></br>
           {routine.day7}{" "}
--- mygainz/src/pages/programs.js
+++ mygainz/src/pages/programs.js
@@ -1,21 +1,14 @@
 import Routine from "../components/Routine.js";
+import routines from "../data/routines.json";
+
 export default function Programs() {
-  const PPL = {
-    name: "PPL",
-    day1: "Chest, Triceps, Shoulders",
-    day2: "Back, Biceps",
-    day3: "Legs",
-    day4: "Chest, Triceps, Shoulders",
-    day5: "Back, Biceps",
-    day6: "Legs",
-    day7: "Rest",
-  };
   return (
     <>
       {" "}
       <h1 className="page-header">Programs</h1>
       <br></br>
-      <Routine routine={PPL} />
+      <Routine routine={routines[0]} />
+      <Routine routine={routines[1]} />
     </>
   );
 }

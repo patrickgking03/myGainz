95231c2a698bae2b7cb96a63d6bca758fd98d246
lprinz1 <notloosh@gmail.com>
2022-12-07 12:59:03 -0800

    updated program data

--- mygainz/src/components/Routine.js
+++ mygainz/src/components/Routine.js
@@ -1,44 +1,13 @@
 export default function Routine({ routine }) {
   return (
-    <div class=" md:w-full bg-gray-200 p-4 rounded-lg outline outline-2 outline-slate-400 py-3  overflow-scroll scrollbar-hide ">
-      <p class="text-gray-900 font-sans capitalize font-medium truncate text-md"></p>
-      <p class="text-gray-400 font-sans capitalize font-regular truncate text-md"></p>
-      {routine.name}
-      <div className="flex-wrap py-8 px-6 flex flex text-center items-center w-full gap-6">
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
-          {" "}
-          Day 1: <br></br> {routine.day1}{" "}
-        </button>
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
-          {" "}
-          Day 2: <br></br>
-          {routine.day2}{" "}
-        </button>
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
-          {" "}
-          Day 3: <br></br>
-          {routine.day3}{" "}
-        </button>
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
-          {" "}
-          Day 4: <br></br>
-          {routine.day4}{" "}
-        </button>
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
-          {" "}
-          Day 5: <br></br>
-          {routine.day5}{" "}
-        </button>
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
-          {" "}
-          Day 6: <br></br>
-          {routine.day6}{" "}
-        </button>
-        <button class="rounded-md text-white bg-orange-400 py-5 px-6 hover:outline outline-2 outline-offset-1 outline-blue-500">
-          {" "}
-          Day 7: <br></br>
-          {routine.day7}{" "}
-        </button>
+    <div class=" md:w-full bg-gray-100 p-8 rounded-lg border border-gray-200 scrollbar-hide space-y-6">
+      <h2 className="text-xl text-gray-700 font-medium">{routine.name}</h2>
+      <div className="flex flex-wrap text-center items-center w-full gap-6">
+        {routine.days.map(day => (
+          <button className="text-sm rounded-md flex-grow text-white bg-blue-500 p-4 hover:outline outline-2 outline-offset-1 outline-blue-500">
+            Day {routine.days.indexOf(day) + 1}: {day.name}
+          </button>
+        ))}
       </div>
       <p class="text-gray-400 font-sans capitalize font-regular truncate text-md mb-4"></p>
     </div>
--- mygainz/src/pages/programs.js
+++ mygainz/src/pages/programs.js
@@ -3,13 +3,9 @@ import routines from "../data/routines.json";
 
 export default function Programs() {
   return (
-    <>
-      {" "}
+    <div className="flex h-full flex-col space-y-6">
       <h1 className="page-header">Programs</h1>
-      <br></br>
       <Routine routine={routines[0]} />
-      <Routine routine={routines[1]} />
-      <Routine routine={routines[2]} />
-    </>
+    </div>
   );
 }

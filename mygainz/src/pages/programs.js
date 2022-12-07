6a44452afd8a8b438d6a6340ceed313b9a19c91e
lprinz1 <notloosh@gmail.com>
2022-12-07 13:08:45 -0800

    cleanup routines

--- mygainz/src/pages/programs.js
+++ mygainz/src/pages/programs.js
@@ -5,7 +5,11 @@ export default function Programs() {
   return (
     <div className="flex h-full flex-col space-y-6">
       <h1 className="page-header">Programs</h1>
-      <Routine routine={routines[0]} />
-    </div>
+      <div className="overflow-scroll scrollbar-hide h-full space-y-6">
+        {routines.map(routine => (
+          <Routine key={routines.indexOf(routine)} routine={routine} />
+        ))}
+      </div>
+    </div >
   );
 }

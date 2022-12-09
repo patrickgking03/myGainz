7405daa13d6ca73bb8ee71c40bec77af67ad25aa
lprinz1 <notloosh@gmail.com>
2022-12-09 15:24:37 -0800

    added modal

--- mygainz/src/components/Routine.js
+++ mygainz/src/components/Routine.js
@@ -1,15 +1,21 @@
-export default function Routine({ routine }) {
-  return (
-    <div class=" md:w-full bg-gray-100 p-8 rounded-lg border border-gray-200 scrollbar-hide space-y-6">
+import RoutineModal from "./RoutineModal";
+
+export default function Routine({ routine, toggleModal, setCurrentDay }) {
+  return <>
+    <div className=" md:w-full bg-gray-100 p-8 rounded-lg border border-gray-200 scrollbar-hide space-y-6">
       <h2 className="text-xl text-gray-700 font-medium">{routine.name}</h2>
       <div className="flex flex-wrap text-center items-center w-full gap-6">
-        {routine.days.map(day => (
-          <button className="text-sm w-48 h-14 rounded-md flex-grow text-white bg-blue-500 p-4 hover:outline outline-2 outline-offset-1 outline-blue-500 flex items-center justify-center">
-            Day {routine.days.indexOf(day) + 1}: {day.name}
-          </button>
-        ))}
+        {routine.days.map(day => {
+          day.index = routine.days.indexOf(day) + 1;
+          return <button onClick={() => {
+            toggleModal();
+            setCurrentDay(day);
+          }} className="text-sm w-48 h-14 rounded-md flex-grow text-white bg-blue-500 p-4 hover:outline outline-2 outline-offset-1 outline-blue-500 flex items-center justify-center">
+            Day {day.index}: {day.name}
+          </button>;
+        })}
       </div>
-      <p class="text-gray-400 font-sans capitalize font-regular truncate text-md mb-4"></p>
+      <p className="text-gray-400 font-sans capitalize font-regular truncate text-md mb-4"></p>
     </div>
-  );
+  </>;
 }
--- mygainz/src/pages/programs.js
+++ mygainz/src/pages/programs.js
@@ -1,15 +1,26 @@
 import Routine from "../components/Routine.js";
+import RoutineModal from "../components/RoutineModal.js";
 import routines from "../data/routines.json";
 
+import { useState } from 'react';
+
 export default function Programs() {
-  return (
+  const [showModal, setShowModal] = useState(false);
+  const [currentDay, setCurrentDay] = useState({})
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  return <>
     <div className="flex h-full flex-col space-y-6">
       <h1 className="page-header">Programs</h1>
       <div className="overflow-scroll scrollbar-hide h-full space-y-6">
         {routines.map(routine => (
-          <Routine key={routines.indexOf(routine)} routine={routine} />
+          <Routine setCurrentDay={setCurrentDay} key={routines.indexOf(routine)} toggleModal={toggleModal} routine={routine} />
         ))}
       </div>
     </div >
-  );
+    {showModal && <RoutineModal currentDay={currentDay} toggleModal={toggleModal} />}
+  </>;
 }

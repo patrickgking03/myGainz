55a80c111b698546925d7ebf2c15d351d2cfa794
lprinz1 <notloosh@gmail.com>
2022-12-12 20:52:50 -0800

    fixed image sizing

--- mygainz/src/components/ExerciseCard.js
+++ mygainz/src/components/ExerciseCard.js
@@ -3,6 +3,6 @@ export default function ExerciseCard({ exercise }) {
     <p className="text-gray-500 font-sans capitalize font-medium truncate text-md">{exercise.name}</p>
     <p className="text-gray-400 font-sans capitalize font-regular truncate text-md">{exercise.equipment}</p>
     <p className="text-gray-400 font-sans capitalize font-regular truncate text-md mb-4">Target Muscle: <span className="text-red-600">{exercise.target}</span></p>
-    <img src={exercise.gifUrl} alt="gif" className="rounded-lg bg-white border-2 border-slate-200" />
+    <img src={exercise.gifUrl} alt="gif" className="rounded-lg bg-white border-2 border-slate-200 aspect-square w-full" />
   </div>;
 }
